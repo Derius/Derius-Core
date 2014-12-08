ba3b8cc22820dfb29054aa05ce641fe1217d0ee4
magnusulf <magnus@puj.dk>
2014-12-08 21:00:49 +0100

    Work on specialisation

---
+++ src/dk/muj/derius/skill/SpecialisationStatus.java
@@ -0,0 +1,25 @@
+package dk.muj.derius.skill;
+
+public enum SpecialisationStatus
+{
+	// -------------------------------------------- //
+	// ENUM
+	// -------------------------------------------- //
+	
+	AUTO_ASSIGNED(),
+	BLACK_LISTED(),
+	HAVE(),
+	DONT_HAVE(),
+	
+	// END OF LIST
+	;
+	
+	// -------------------------------------------- //
+	// CONSTRUCT
+	// -------------------------------------------- //
+	
+	SpecialisationStatus()
+	{
+		
+	}
+}
