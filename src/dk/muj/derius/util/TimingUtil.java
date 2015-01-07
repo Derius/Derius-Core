bcf952265ee4c84f64ab83ef2f41add826b40ea4
ulumulu1510 <beniheusser@gmail.com>
2015-01-07 20:48:35 +0100

    end work TimingUtil in a static version, will change to non-static

--- src/dk/muj/derius/util/TimingUtil.java
+++ src/dk/muj/derius/util/TimingUtil.java
@@ -1,18 +1,24 @@
 package dk.muj.derius.util;
 
 import com.massivecraft.massivecore.MassivePlugin;
+import com.massivecraft.massivecore.util.Txt;
 
 import dk.muj.derius.Derius;
 import dk.muj.derius.entity.MConf;
 
 public class TimingUtil
-{		
+{	
 	// -------------------------------------------- //
 	// FIELDS
 	// -------------------------------------------- //
 	
+	/*
+	 * We store this info for the logging, that might happen. 
+	 * Without this info, it would be impossible to keep track
+	 * of multiple timings in the project.
+	 */
 	private static long startTime;
-	private static String declaringclass = "";
+	private static String declaringClass = "";
 	private static String methodName = "";
 	
 	
@@ -20,22 +26,38 @@ public class TimingUtil
 	// TIMER CONTROLE
 	// -------------------------------------------- //	
 	
-	public static void startTiming()
+	/**
+	 * Starts the timing process.
+	 * @param {String} The Name of the class this timer is placed.
+	 * @param {String} The Name of the method this timer is placed.
+	 */
+	public static void startTiming(String className, String methodName)
 	{
+		TimingUtil.declaringClass = className;
+		TimingUtil.methodName = methodName;
+		
 		TimingUtil.startTime = System.nanoTime();
 	}
 	
-	public static void endTiming()
+	/**
+	 * Ends the timing process and creates a log if the time
+	 * is over the timingMax in MConf.
+	 * @param {String} The Name of the class this timer is placed.
+	 * @param {String} The Name of the method this timer is placed.
+	 */
+	public static void endTiming(String className, String methodName)
 	{
+		if (TimingUtil.hasError(className, methodName)) { return; }
+		
 		long startTime = TimingUtil.startTime;
-		long endTime= System.nanoTime();
+		long endTime = System.nanoTime();
 		
 		double difference = (endTime - startTime) / 1000_000.0;
 		
 		if (difference > MConf.get().timingMax)
 		{
 			MassivePlugin p = Derius.get();
-			p.log("");
+			p.log(Txt.parse("<i>The Timing in class <lime>%s <i>and method <lime>%s <i>has shown a run time of <lime>&s <i>seconds. It might be of value to take a look at it.", TimingUtil.declaringClass, TimingUtil.methodName, difference));
 		}
 	}
 	
@@ -43,5 +65,15 @@ public class TimingUtil
 	// PRIVAT
 	// -------------------------------------------- //
 
-
+	private static boolean hasError (String className, String methodName)
+	{
+		if (TimingUtil.declaringClass != className || TimingUtil.methodName != methodName) 
+		{ 
+			return true; 
+		}
+		else
+		{
+			return false;
+		}	
+	}
 }
